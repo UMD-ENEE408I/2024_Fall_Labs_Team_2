cacfb5066e2b4e191051da089ac52f8fa495008c
eguzz313 <145940761+eguzz313@users.noreply.github.com>
2024-09-11 19:22:08 -0400

    Update IMU code

--- 2024_Fall_Lab_1-main/2024_Fall_Lab_1-main/Tutorial_1/IMU/src/main.cpp
+++ 2024_Fall_Lab_1-main/2024_Fall_Lab_1-main/Tutorial_1/IMU/src/main.cpp
@@ -9,10 +9,11 @@ Adafruit_MPU6050 mpu;
 const unsigned int ADC_1_CS = 2;
 const unsigned int ADC_2_CS = 17;
 
+float angle_z = 0;  // Store the accumulated rotation angle
+unsigned long previous_time = 0;
+
 void setup(void) {
   // Stop the right motor by setting pin 14 low
-  // this pin floats high or is pulled
-  // high during the bootloader phase for some reason
   pinMode(14, OUTPUT);
   digitalWrite(14, LOW);
   delay(100);
@@ -26,10 +27,10 @@ void setup(void) {
   pinMode(ADC_1_CS, OUTPUT);
   pinMode(ADC_2_CS, OUTPUT);
 
-  digitalWrite(ADC_1_CS, HIGH); // Without this the ADC's write
-  digitalWrite(ADC_2_CS, HIGH); // to the SPI bus while the nRF24 is!!!!
+  digitalWrite(ADC_1_CS, HIGH); 
+  digitalWrite(ADC_2_CS, HIGH); 
 
-  // Try to initialize!
+  // Try to initialize MPU6050!
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -39,66 +40,13 @@ void setup(void) {
   Serial.println("MPU6050 Found!");
 
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
 
   Serial.println("");
   delay(100);
+
+  previous_time = millis();  // Record the initial time
 }
 
 void loop() {
@@ -107,7 +55,7 @@ void loop() {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  /* Print out the values */
+  /* Print out the sensor values */
   Serial.print("Acceleration X: ");
   Serial.print(a.acceleration.x);
   Serial.print(", Y: ");
@@ -127,4 +75,26 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
   Serial.println(" degC");
+
+  // Calculate elapsed time
+  unsigned long current_time = millis();
+  float time_diff = (current_time - previous_time) / 1000.0;  // Convert to seconds
+  previous_time = current_time;
+
+  // Integrate the angular velocity (gyroscope Z-axis) to get angle in degrees
+  angle_z += g.gyro.z * time_diff * (180 / PI);  // rad/s to degrees
+
+  Serial.print("Accumulated Z Angle: ");
+  Serial.println(angle_z);
+
+  // Rotate motor 90 degrees
+  if (angle_z < 90) {
+    analogWrite(14, 100); 
+  } else if (angle_z < 180) {
+    analogWrite(14, 100);  // Keep moving until 180 degrees
+  } else {
+    analogWrite(14, 0);  // Stop motor after 180 degrees
+  }
+
+  delay(100);
 }
